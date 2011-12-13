5c2f5a8fcb294699cdc547615c145264ab6b027e
unknown <Greg@.(none)>
2011-12-13 22:23:41 +0000

    Initial import. No work done yet.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Podcatcher::Application.config.secret_token = '1fabf51c7fec0fd9ebfd7257dfd27ef9a02c726736aa42672993035df895e0dec7130d7119aea811595fb1c11a78665393ebc7dbf4161c57cfde2b9d4a749029'
