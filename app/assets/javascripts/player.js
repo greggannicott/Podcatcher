86de96db51b199f95f0924c22ed50811f3d8235d
Greg Gannicott <greg@gannicott.co.uk>
2011-12-21 12:33:51 +0000

    Audio Player now plays from Episode.current_time.

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -3,8 +3,16 @@ $(document).ready(function() {
   var player = $("#player").get(0);
 
   // Check for current time every 2 seconds
-  setInterval("noteCurrentTime()", 22000);
-  //setInterval("noteCurrentTime()", 2000);
+  //setInterval("noteCurrentTime()", 22000);
+  setInterval("noteCurrentTime()", 2000);
+
+  // Set the audio to a certain point when the audio element has loaded
+  $("#player").bind('canplay', function() {
+    // Determine how far into the podcast the user is
+    previous_current_time = $("#player").data("previous-current-time");
+    player.currentTime = previous_current_time;
+  });
+
 
 });
 
