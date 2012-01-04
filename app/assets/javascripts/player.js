352264d29e78ae9a740df82eb0fd416369bc0bec
Greg Gannicott <greg@gannicott.co.uk>
2012-01-04 13:19:32 +0000

    episodes#show now has a default installation of jPlayer included, rather than a HTML5 player.

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -6,27 +6,37 @@ $(document).ready(function() {
 
   // Check for current time every 2 seconds
   //setInterval("noteCurrentTime()", 22000);
-  setInterval("noteCurrentTime()", 2000);
+  //setInterval("noteCurrentTime()", 2000);
 
   // Set the audio to a certain point when the audio element has loaded
-  $("#player").bind('canplay', function() {
+  //$("#player").bind('canplay', function() {
     // Determine how far into the podcast the user is
-    previous_current_time = $("#player").data("previous-current-time");
-    seekable = player.seekable.end();
-    player.currentTime = previous_current_time;
+    //previous_current_time = $("#player").data("previous-current-time");
+    //seekable = player.seekable.end();
+    //player.currentTime = previous_current_time;
     // Note that it's now safe to note the current play time
     // We wouldn't want to do it before the 'can_play' event as we'd be noting
     // 0 seconds and over-writing a previous value.
-    can_note = true;
+    //can_note = true;
     //alert(previous_current_time);
     //noteCurrentTime();
-  });
+  //});
 
   // Note the current time when the user presses pause
-  $("#player").bind('pause', function() {
-    noteCurrentTime();
-  });
-  
+  //$("#player").bind('pause', function() {
+    //noteCurrentTime();
+  //});
+ 
+      $("#jquery_jplayer_1").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+            oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+          });
+        },
+        swfPath: "/assets",
+        supplied: "m4a, oga"
+      });
 
   // Capture the currentTime if the user closes the window
 //  $(window).bind('beforeunload', function() {
